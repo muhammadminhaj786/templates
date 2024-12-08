4223efe20a6da468bcb13485fb1c5de3f62e6a22
muhammadminhaj786 <eb20103087.minhajwahid@gmail.com>
2024-12-08 21:21:54 +0500

    creating signup page

---
+++ frontend_templates/src/pages/SignUp.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const SignUp = () => {
+  return (
+    <div className='border-2 border-black h-screen w-full flex '>
+        <div className='grid w-full grid-cols-1 items-center justify-center md:grid-cols-2 border-2 border-red-800'>
+            <div className='hidden md:flex items-center justify-center'>
+                <img src="../src\assets\images\lOGO.png" alt="" width={400} />
+            </div>
+            <form className='min-h-screen w-full '>
+
+            </form>
+        </div>
+    </div>
+  )
+}
+
+export default SignUp
